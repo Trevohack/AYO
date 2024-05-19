ce1768dece27c931e2c18c4af17be3f6c7c95bd4
Treveen <136177431+Trevohack@users.noreply.github.com>
2024-05-18 20:00:18 -0500

    AYO - 2.0

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
+# Author: Trevohack
 
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
-RESET='\033[0m' 
+RESET='\033[0m'
 
 
 banner() {
-    echo -e """
-$RED 
-
+    echo -e "${RED}
  ▄▄▄     ▓██   ██▓ ▒█████  
 ▒████▄    ▒██  ██▒▒██▒  ██▒
 ▒██  ▀█▄   ▒██ ██░▒██░  ██▒
@@ -22,30 +21,31 @@ $RED
   ░   ▒   ▒ ▒ ░░  ░ ░ ░ ▒  
       ░  ░░ ░         ░ ░  
           ░ ░              
-
-$RESET
-    """
+${RESET}"
+    read -p "Enter your username: " name
 }
 
+
 init() {
-    echo -e "[$GREEN+$RESET] Configuring Environment..."
-    mkdir ~/Documents/ayo
-    cd ~/Documents/ayo 
+    echo -e "[${GREEN}+${RESET}] Configuring Environment..."
+    mkdir -p "/home/$name/Documents/ayo"
+    cd "/home/$name/Documents/ayo" || exit
 }
 
 install() {
-    echo -e "[$GREEN+$RESET] Downloading AYO..."
-    wget https://raw.githubusercontent.com/Trevohack/AYO/main/src/main.py -O ~/Documents/ayo/main.py 
-    wget https://raw.githubusercontent.com/Trevohack/AYO/main/src/machine_data.json -O ~/Documents/ayo/machine_data.json
+    echo -e "[${GREEN}+${RESET}] Downloading AYO..."
+    wget https://raw.githubusercontent.com/Trevohack/AYO/main/src/main.py -O "/home/$name/Documents/ayo/main.py"
+    wget https://raw.githubusercontent.com/Trevohack/AYO/main/src/machine_data.json -O "/home/$name/Documents/ayo/machine_data.json"
 
-    sudo ~/Documents/ayo/main.py /usr/bin/ayo 
+    sudo cp "/home/$name/Documents/ayo/main.py" "/usr/bin/ayo" 
     sudo chmod +x /usr/bin/ayo 
 }
 
 end() {
-    echo -e "[$GREEN+$RESET] Happy Hacking!" 
+    echo -e "[${GREEN}+${RESET}] Happy Hacking!"
 }
 
-banner 
+banner
 init
-install 
+install
+end
